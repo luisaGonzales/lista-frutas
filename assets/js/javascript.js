20e4a28da40b5aed37fc443b08515e4c21ff785c
luisaGonzales <luisagc711@gmail.com>
2017-07-14 00:30:08 -0500

    Código JS completo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,18 +1,39 @@
+//Almacenamiento de valores y variables
 var frutas = [];
 var lista = document.getElementById("lista");
-
-//Cuando hacen click
+//Cuando hacen click y agregar funcionalidad
 var addFruta = document.getElementById("agregar");
 addFruta.onclick = function(){
   var nuevaFruta = document.getElementById("frutas").value;
-  frutas.push(nuevaFruta);
+  nuevaFruta = nuevaFruta.charAt(0).toUpperCase() + nuevaFruta.slice(1).toLowerCase();
+  var frutaRepetida = false;
+  for(var i = 0; i < frutas.length; i++){
+    if(nuevaFruta == frutas[i]){
+      frutaRepetida = true;
+      alert("Esta fruta ya se encuentra en el catálogo");
+      document.getElementById("frutas").value = "";
+      break;
+    }
+  }
+  if (!isNaN(parseInt(nuevaFruta))){
+    alert("Los números no se llevan bien con las frutas");
+    document.getElementById("frutas").value = "";
+  } else if ((nuevaFruta == " ") || (nuevaFruta == "")){
+    alert("Esa fruta no sabe a nada, intenta otra vez");
+    document.getElementById("frutas").value = "";
+  }
+  else if (frutaRepetida == false) {
+    frutas.push(nuevaFruta);
+    document.getElementById("frutas").value = "";
+    lista.innerHTML = mostrar(frutas);
+  }
 }
-
- function mostrar (arr){
-   var html = "";
-   for (var i = 0; i < frutas.length; i++){
-     var id = i + 1;
-     html += id + ". " + frutas[i];
-   }
-   lista.innerHTML = html;
- }
+//Función para mostrar
+function mostrar (arr){
+  var html = "";
+  for (var i = 0; i < frutas.length; i++){
+    var id = i + 1;
+    html += id + ". " + frutas[i] + "</br>";
+  }
+  return html;
+}
